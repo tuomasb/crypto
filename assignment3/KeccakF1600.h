61127304d32cbde9bfe7738b3170581f4f24cbb1
Tuomas Blomqvist <tuomas.blomqvist@aalto.fi>
2013-11-18 10:00:42 +0200

    Assignment 3 template

---
+++ assignment3/KeccakF1600.h
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+
+unsigned long**  KeccakF(unsigned long** A);
+
+
+unsigned char* KeccakF_bytes(unsigned char *state);
+
---
+++ assignment3/stream.c
@@ -0,0 +1,16 @@
+#include "stream.h"
+
+
+//Initialisation function
+// The key is of lenght 16 bytes and will corresponds to the 16 first bytes of the 1600-bits state S. The others bytes correspond to the initial value. Here 0. 
+void initialisation(unsigned char *S,unsigned char* key){
+  
+}
+
+
+// This encryption function return a ciphertext of lenght lenght corresponding to the plaintext Plaintext. The key is 16-bytes. 
+unsigned char* encrypt( unsigned char* key, unsigned char* Plaintext,unsigned int lenght){
+ 
+}
+
+
